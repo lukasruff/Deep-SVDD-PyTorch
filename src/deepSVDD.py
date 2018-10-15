abcffac21271653a852a36e622a2420342da2e3b
lukasruff <lukas.ruff@gmail.com>
2018-10-15 12:54:28 +0200

    Add Dataset and DataLoader attributes to DeepSVDD class

--- src/deepSVDD.py
+++ src/deepSVDD.py
@@ -1,18 +1,28 @@
+from torch.utils.data import DataLoader
+from datasets.main import load_dataset
+
+
 class DeepSVDD(object):
     """A class for the Deep SVDD method.
 
     Attributes:
-        dataset: A string indicating the dataset to load.
+        dataset_name: A string indicating the name of the dataset to load.
+        dataset: The Dataset.
+        data: The DataLoader for the dataset.
         model: A string indicating the model to use.
     """
 
-    def __init__(self, dataset, model):
-        """Inits DeepSVDD with dataset and model."""
-        self.dataset = dataset
+    def __init__(self, dataset_name, model):
+        """Inits DeepSVDD with data and model."""
+        self.dataset_name = dataset_name
+        self.dataset = None
+        self.data = None
         self.model = model
 
     def load_data(self):
-        """ Loads the dataset"""
+        """ Loads the data"""
+        self.dataset = load_dataset(self.dataset_name)
+        self.data = DataLoader(self.dataset)
         pass
 
     def train(self):
