694041357825948ad864044794531e24474b585b
lukasruff <lukas.ruff@gmail.com>
2018-10-15 17:54:30 +0200

    Implementation of dataset base classes

---
+++ src/datasets/torchvision_dataset.py
@@ -0,0 +1,17 @@
+from .base import MyDataset
+from torch.utils.data import DataLoader
+
+
+class TorchvisionDataset(MyDataset):
+    """TorchvisionDataset class for datasets already implemented in torchvision.datasets."""
+
+    def __init__(self, root: str):
+        super().__init__(root)
+
+    def loaders(self, batch_size: int, shuffle_train=True, shuffle_test=False, num_workers: int = 0) -> (
+            DataLoader, DataLoader):
+        train_loader = DataLoader(dataset=self.train_set, batch_size=batch_size, shuffle=shuffle_train,
+                                  num_workers=num_workers)
+        test_loader = DataLoader(dataset=self.test_set, batch_size=batch_size, shuffle=shuffle_test,
+                                 num_workers=num_workers)
+        return train_loader, test_loader
