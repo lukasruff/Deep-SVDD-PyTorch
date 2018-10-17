b2d164535e3e94316f6d9ec660138b0c4734a430
lukasruff <lukas.ruff@gmail.com>
2018-10-17 16:01:13 +0200

    Implemented simple LeNet models for CIFAR-10 and MNIST.

---
+++ src/models/__init__.py
@@ -0,0 +1,2 @@
+from .mnist_LeNet import MNIST_LeNet
+from .cifar10_LeNet import CIFAR10_LeNet
---
+++ src/models/cifar10_LeNet.py
@@ -0,0 +1,28 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+from base.base_model import BaseModel
+
+
+class CIFAR10_LeNet(BaseModel):
+
+    def __init__(self):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+        self.pool = nn.MaxPool2d(2, 2)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
