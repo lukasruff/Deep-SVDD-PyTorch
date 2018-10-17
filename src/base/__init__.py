735db02f66b1bda791849d334bfea4b5b69efb5a
lukasruff <lukas.ruff@gmail.com>
2018-10-17 16:00:33 +0200

    Implementation of base class for models.

---
+++ src/base/__init__.py
@@ -0,0 +1,3 @@
+from .base_dataset import *
+from .torchvision_dataset import *
+from .base_model import *
---
+++ src/base/base_model.py
@@ -0,0 +1,25 @@
+import logging
+import torch.nn as nn
+import numpy as np
+
+
+class BaseModel(nn.Module):
+    """Base class for all models."""
+
+    def __init__(self):
+        super().__init__()
+        self.logger = logging.getLogger(self.__class__.__name__)
+
+    def forward(self, *input):
+        """
+        Forward pass logic
+        :return: Model output
+        """
+        raise NotImplementedError
+
+    def summary(self):
+        """Model summary."""
+        model_parameters = filter(lambda p: p.requires_grad, self.parameters())
+        params = sum([np.prod(p.size()) for p in model_parameters])
+        self.logger.info('Trainable parameters: {}'.format(params))
+        self.logger.info(self)
