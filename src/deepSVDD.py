91527a0378f8e5662646ac37fbc2348efe81d81f
lukasruff <lukas.ruff@gmail.com>
2018-10-19 15:54:26 +0200

    Updated DeepSVDD class.
    Removed data as class attribute. Data is now passed as a parameter to
    .train() and .test() method.
    Added objective, nu, c, and R as attributes.
    Added .set_model(), .train(), and .test() method.

--- src/deepSVDD.py
+++ src/deepSVDD.py
@@ -1,4 +1,6 @@
-from datasets.main import load_dataset
+from base.base_dataset import BaseADDataset
+from models.main import build_model
+from optim.deepSVDD_trainer import DeepSVDDTrainer
 
 
 class DeepSVDD(object):
@@ -11,20 +13,38 @@ class DeepSVDD(object):
         model: The model.
     """
 
-    def __init__(self, dataset_name, model_name):
+    def __init__(self, objective: str = 'one-class', nu: float = 0.1):
         """Inits DeepSVDD with data and model."""
-        self.dataset_name = dataset_name
-        self.data = None
+
+        assert objective in ('one-class', 'soft-boundary'), "Objective must be either 'one-class' or 'soft-boundary'."
+        self.objective = objective
+        assert (0 < nu) & (nu <= 1), "For hyperparameter nu, it must hold: 0 < nu <= 1."
+        self.nu = nu
+        self.R = 0  # hypersphere radius R
+        self.c = None  # hypersphere center c
+
+        self.model_name = None
+        self.model = None  # neural network model \phi
+
+        self.trainer = None
+        self.optimizer_name = None
+
+    def set_model(self, model_name):
+        """Builds the model."""
         self.model_name = model_name
-        self.model = None
+        self.model = build_model(model_name)
 
-        # load data
-        self.load_data()
+    def train(self, dataset: BaseADDataset, optimizer_name, lr: float = 0.001, n_epochs: int = 100,
+              batch_size: int = 128):
+        """Trains the model on the training data."""
 
-    def load_data(self):
-        """ Loads the data"""
-        self.data = load_dataset(self.dataset_name)
+        self.optimizer_name = optimizer_name
 
-    def train(self):
-        """Trains the model"""
-        pass
+        self.trainer = DeepSVDDTrainer(self.objective, optimizer_name,
+                                       lr=lr, n_epochs=n_epochs, batch_size=batch_size, nu=self.nu)
+        self.model = self.trainer.train(dataset, self.model)
+
+    def test(self, dataset: BaseADDataset):
+        """Tests the model on the test data."""
+
+        self.trainer.test(dataset, self.model)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import click
 
 from deepSVDD import DeepSVDD
+from datasets.main import load_dataset
 
 
 @click.command()
@@ -8,7 +9,17 @@ from deepSVDD import DeepSVDD
 @click.argument('model_name')
 def main(dataset_name, model_name):
 
-    deep_SVDD = DeepSVDD(dataset_name, model_name)
+    # Load data
+    dataset = load_dataset(dataset_name)
+
+    # Initialize DeepSVDD with model
+    # deep_SVDD = DeepSVDD(objective='soft-boundary', nu=0.01)
+    deep_SVDD = DeepSVDD()
+    deep_SVDD.set_model(model_name)
+
+    # Train model on dataset
+    deep_SVDD.train(dataset, optimizer_name='adam', lr=0.0001, n_epochs=25, batch_size=200)
+    deep_SVDD.test(dataset)
 
 
 if __name__ == '__main__':
