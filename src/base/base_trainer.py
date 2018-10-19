1080280711f16588376a143922d37f0f3a19fb12
lukasruff <lukas.ruff@gmail.com>
2018-10-19 15:49:05 +0200

    Implemented base Trainer class with .train() and .test() methods.

---
+++ src/base/base_trainer.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+from .base_dataset import BaseADDataset
+from .base_model import BaseModel
+
+
+class BaseTrainer(ABC):
+    """Trainer base class."""
+
+    def __init__(self, optimizer_name: str, lr: float, n_epochs: int, batch_size: int, n_jobs_dataloader: int):
+        super().__init__()
+        self.optimizer_name = optimizer_name
+        self.lr = lr
+        self.n_epochs = n_epochs
+        self.batch_size = batch_size
+        self.n_jobs_dataloader = n_jobs_dataloader
+
+    @abstractmethod
+    def train(self, dataset: BaseADDataset, model: BaseModel) -> BaseModel:
+        """
+        Implement train method that trains the given model using the train_set of dataset.
+        :return: Trained model
+        """
+        pass
+
+    @abstractmethod
+    def test(self, dataset: BaseADDataset, model: BaseModel):
+        """
+        Implement test method that evaluates the test_set of dataset on the given model.
+        """
+        pass
