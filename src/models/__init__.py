b1bdf9b6ab3251d36daec4021c7bb979af9df5e3
lukasruff <lukas.ruff@gmail.com>
2018-10-23 09:01:52 +0200

    Implemented respective autoencoder network for simple MNIST LeNet.

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,2 +1,2 @@
-from .mnist_LeNet import MNIST_LeNet
+from .mnist_LeNet import MNIST_LeNet, MNIST_LeNet_Autoencoder
 from .cifar10_LeNet import CIFAR10_LeNet
--- src/models/mnist_LeNet.py
+++ src/models/mnist_LeNet.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -28,4 +29,47 @@ class MNIST_LeNet(BaseModel):
         x = self.pool(F.leaky_relu(self.bn2(x)))
         x = x.view(x.size(0), -1)
         x = self.fc1(x)
+        return x
+
+
+class MNIST_LeNet_Autoencoder(BaseModel):
+
+    def __init__(self):
+        super().__init__()
+
+        self.rep_dim = 32
+
+        self.conv1 = nn.Conv2d(1, 8, 5, bias=False, padding=2)
+        self.bn1 = nn.BatchNorm2d(8)
+
+        self.conv2 = nn.Conv2d(8, 4, 5, bias=False, padding=2)
+        self.bn2 = nn.BatchNorm2d(4)
+
+        self.code = nn.Linear(4 * 7 * 7, self.rep_dim, bias=False)
+
+        self.conv3 = nn.Conv2d(2, 4, 5, bias=False, padding=2)
+        self.bn3 = nn.BatchNorm2d(4)
+
+        self.conv4 = nn.Conv2d(4, 8, 5, bias=False, padding=1)
+        self.bn4 = nn.BatchNorm2d(8)
+
+        self.conv5 = nn.Conv2d(8, 1, 5, bias=False, padding=2)
+
+        self.pool = nn.MaxPool2d(2, 2)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.pool(F.leaky_relu(self.bn1(x)))
+        x = self.conv2(x)
+        x = self.pool(F.leaky_relu(self.bn2(x)))
+        x = x.view(x.size(0), -1)
+        x = self.code(x)
+        x = x.view(x.size(0), int(self.rep_dim / 16), 4, 4)
+        x = F.interpolate(F.leaky_relu(x), scale_factor=2)
+        x = self.conv3(x)
+        x = F.interpolate(F.leaky_relu(self.bn3(x)), scale_factor=2)
+        x = self.conv4(x)
+        x = F.interpolate(F.leaky_relu(self.bn4(x)), scale_factor=2)
+        x = self.conv5(x)
+        x = torch.sigmoid(x)
         return x
