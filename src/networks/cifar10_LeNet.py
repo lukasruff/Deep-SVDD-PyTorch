0d5d83ac5d86e718b705a2e2deb5e514bd899cf3
lukasruff <lukas.ruff@gmail.com>
2018-10-29 16:13:38 +0100

    Updated network architectures.

--- src/networks/cifar10_LeNet.py
+++ src/networks/cifar10_LeNet.py
@@ -10,15 +10,13 @@ class CIFAR10_LeNet(BaseNet):
         super().__init__()
 
         self.rep_dim = 64
+        self.pool = nn.MaxPool2d(2, 2)
 
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.conv2 = nn.Conv2d(6, 16, 5)
-
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, self.rep_dim)
-
-        self.pool = nn.MaxPool2d(2, 2)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
--- src/networks/mnist_LeNet.py
+++ src/networks/mnist_LeNet.py
@@ -11,16 +11,13 @@ class MNIST_LeNet(BaseNet):
         super().__init__()
 
         self.rep_dim = 32
+        self.pool = nn.MaxPool2d(2, 2)
 
         self.conv1 = nn.Conv2d(1, 8, 5, bias=False, padding=2)
         self.bn1 = nn.BatchNorm2d(8)
-
         self.conv2 = nn.Conv2d(8, 4, 5, bias=False, padding=2)
         self.bn2 = nn.BatchNorm2d(4)
-
         self.fc1 = nn.Linear(4 * 7 * 7, self.rep_dim, bias=False)
-
-        self.pool = nn.MaxPool2d(2, 2)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -38,24 +35,21 @@ class MNIST_LeNet_Autoencoder(BaseNet):
         super().__init__()
 
         self.rep_dim = 32
+        self.pool = nn.MaxPool2d(2, 2)
 
+        # Encoder (must match the Deep SVDD network above)
         self.conv1 = nn.Conv2d(1, 8, 5, bias=False, padding=2)
         self.bn1 = nn.BatchNorm2d(8)
-
         self.conv2 = nn.Conv2d(8, 4, 5, bias=False, padding=2)
         self.bn2 = nn.BatchNorm2d(4)
+        self.fc1 = nn.Linear(4 * 7 * 7, self.rep_dim, bias=False)
 
-        self.code = nn.Linear(4 * 7 * 7, self.rep_dim, bias=False)
-
+        # Decoder
         self.conv3 = nn.Conv2d(2, 4, 5, bias=False, padding=2)
         self.bn3 = nn.BatchNorm2d(4)
-
         self.conv4 = nn.Conv2d(4, 8, 5, bias=False, padding=1)
         self.bn4 = nn.BatchNorm2d(8)
-
         self.conv5 = nn.Conv2d(8, 1, 5, bias=False, padding=2)
-
-        self.pool = nn.MaxPool2d(2, 2)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -63,7 +57,7 @@ class MNIST_LeNet_Autoencoder(BaseNet):
         x = self.conv2(x)
         x = self.pool(F.leaky_relu(self.bn2(x)))
         x = x.view(x.size(0), -1)
-        x = self.code(x)
+        x = self.fc1(x)
         x = x.view(x.size(0), int(self.rep_dim / 16), 4, 4)
         x = F.interpolate(F.leaky_relu(x), scale_factor=2)
         x = self.conv3(x)
