8b08f978c09a1521713d76b98301f0ad731f6fc0
lukasruff <lukas.ruff@gmail.com>
2018-11-21 09:08:22 +0100

    Add learnable BatchNorm parameters again for testing.

--- src/networks/cifar10_LeNet.py
+++ src/networks/cifar10_LeNet.py
@@ -14,11 +14,11 @@ class CIFAR10_LeNet(BaseNet):
         self.pool = nn.MaxPool2d(2, 2)
 
         self.conv1 = nn.Conv2d(3, 32, 5, bias=False, padding=2)
-        self.bn2d1 = nn.BatchNorm2d(32, eps=1e-04, affine=False)
+        self.bn2d1 = nn.BatchNorm2d(32, eps=1e-04)
         self.conv2 = nn.Conv2d(32, 64, 5, bias=False, padding=2)
-        self.bn2d2 = nn.BatchNorm2d(64, eps=1e-04, affine=False)
+        self.bn2d2 = nn.BatchNorm2d(64, eps=1e-04)
         self.conv3 = nn.Conv2d(64, 128, 5, bias=False, padding=2)
-        self.bn2d3 = nn.BatchNorm2d(128, eps=1e-04, affine=False)
+        self.bn2d3 = nn.BatchNorm2d(128, eps=1e-04)
         self.fc1 = nn.Linear(128 * 4 * 4, self.rep_dim, bias=False)
 
     def forward(self, x):
@@ -43,20 +43,20 @@ class CIFAR10_LeNet_Autoencoder(BaseNet):
 
         # Encoder (must match the Deep SVDD network above)
         self.conv1 = nn.Conv2d(3, 32, 5, bias=False, padding=2)
-        self.bn2d1 = nn.BatchNorm2d(32, eps=1e-04, affine=False)
+        self.bn2d1 = nn.BatchNorm2d(32, eps=1e-04)
         self.conv2 = nn.Conv2d(32, 64, 5, bias=False, padding=2)
-        self.bn2d2 = nn.BatchNorm2d(64, eps=1e-04, affine=False)
+        self.bn2d2 = nn.BatchNorm2d(64, eps=1e-04)
         self.conv3 = nn.Conv2d(64, 128, 5, bias=False, padding=2)
-        self.bn2d3 = nn.BatchNorm2d(128, eps=1e-04, affine=False)
+        self.bn2d3 = nn.BatchNorm2d(128, eps=1e-04)
         self.fc1 = nn.Linear(128 * 4 * 4, self.rep_dim, bias=False)
 
         # Decoder
         self.conv4 = nn.Conv2d(int(self.rep_dim / (4 * 4)), 128, 5, bias=False, padding=2)
-        self.bn2d4 = nn.BatchNorm2d(128, eps=1e-04, affine=False)
+        self.bn2d4 = nn.BatchNorm2d(128, eps=1e-04)
         self.conv5 = nn.Conv2d(128, 64, 5, bias=False, padding=2)
-        self.bn2d5 = nn.BatchNorm2d(64, eps=1e-04, affine=False)
+        self.bn2d5 = nn.BatchNorm2d(64, eps=1e-04)
         self.conv6 = nn.Conv2d(64, 32, 5, bias=False, padding=2)
-        self.bn2d6 = nn.BatchNorm2d(32, eps=1e-04, affine=False)
+        self.bn2d6 = nn.BatchNorm2d(32, eps=1e-04)
         self.conv7 = nn.Conv2d(32, 3, 5, bias=False, padding=2)
 
     def forward(self, x):
