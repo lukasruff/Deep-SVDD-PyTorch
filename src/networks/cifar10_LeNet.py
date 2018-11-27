ff6107e71d37566814dceb24709eab394864f70f
lukasruff <lukas.ruff@gmail.com>
2018-11-27 10:11:07 +0100

    Added BatchNorm to code layer in autoencoder pre-training (fix).

--- src/networks/cifar10_LeNet.py
+++ src/networks/cifar10_LeNet.py
@@ -76,7 +76,7 @@ class CIFAR10_LeNet_Autoencoder(BaseNet):
         x = self.conv3(x)
         x = self.pool(F.leaky_relu(self.bn2d3(x)))
         x = x.view(x.size(0), -1)
-        x = self.fc1(x)
+        x = self.bn1d(self.fc1(x))
         x = x.view(x.size(0), int(self.rep_dim / (4 * 4)), 4, 4)
         x = F.leaky_relu(x)
         x = self.conv4(x)
