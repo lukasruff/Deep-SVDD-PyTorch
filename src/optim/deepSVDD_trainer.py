Fixed computation of radius since dist is (x - center) ** 2

src/optim/deepSVDD_trainer.py
@@ -178,4 +178,4 @@ class DeepSVDDTrainer(BaseTrainer):
 
 def get_radius(dist: torch.Tensor, nu: float):
     """Optimally solve for radius R via the (1-nu)-quantile of distances."""
-    return np.quantile(dist.clone().data.cpu().numpy(), 1-nu)
+    return np.quantile(np.sqrt(dist.clone().data.cpu().numpy()), 1 - nu)