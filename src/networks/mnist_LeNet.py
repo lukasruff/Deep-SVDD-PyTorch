f31104024769d37dcf28c493f30cddb2f2721950
lukasruff <lukas.ruff@gmail.com>
2019-04-01 16:12:33 +0200

    Updated MNIST and CIFAR-10 CAE decoder networks with ConvTranspose2d.

--- src/networks/mnist_LeNet.py
+++ src/networks/mnist_LeNet.py
@@ -45,11 +45,11 @@ class MNIST_LeNet_Autoencoder(BaseNet):
         self.fc1 = nn.Linear(4 * 7 * 7, self.rep_dim, bias=False)
 
         # Decoder
-        self.conv3 = nn.Conv2d(2, 4, 5, bias=False, padding=2)
+        self.deconv1 = nn.ConvTranspose2d(2, 4, 5, bias=False, padding=2)
         self.bn3 = nn.BatchNorm2d(4, eps=1e-04, affine=False)
-        self.conv4 = nn.Conv2d(4, 8, 5, bias=False, padding=1)
+        self.deconv2 = nn.ConvTranspose2d(4, 8, 5, bias=False, padding=3)
         self.bn4 = nn.BatchNorm2d(8, eps=1e-04, affine=False)
-        self.conv5 = nn.Conv2d(8, 1, 5, bias=False, padding=2)
+        self.deconv3 = nn.ConvTranspose2d(8, 1, 5, bias=False, padding=2)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -60,10 +60,11 @@ class MNIST_LeNet_Autoencoder(BaseNet):
         x = self.fc1(x)
         x = x.view(x.size(0), int(self.rep_dim / 16), 4, 4)
         x = F.interpolate(F.leaky_relu(x), scale_factor=2)
-        x = self.conv3(x)
+        x = self.deconv1(x)
         x = F.interpolate(F.leaky_relu(self.bn3(x)), scale_factor=2)
-        x = self.conv4(x)
+        x = self.deconv2(x)
         x = F.interpolate(F.leaky_relu(self.bn4(x)), scale_factor=2)
-        x = self.conv5(x)
+        x = self.deconv3(x)
         x = torch.sigmoid(x)
+
         return x
